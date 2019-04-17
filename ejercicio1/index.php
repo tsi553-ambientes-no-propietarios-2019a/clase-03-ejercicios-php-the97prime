3f46b93bda5bcff5739eda6404f0cab93775c3d9
Oscar Mateo Peñaherrera <osmap_1997@gmail.com>
2019-04-16 19:01:11 -0500

    Ejercicios Tabla de multiplicar

--- ejercicio1/index.php
+++ ejercicio1/index.php
@@ -4,4 +4,13 @@
  * Mostrar la tabla de multiplicar del 2. Emplear el for, luego el while y por último el do/while.
  * La estructura for permite incrementar una variable de 2 en 2
  */
+for($tabla=2; $tabla<=2; $tabla++) 
+{
+ echo "<h3>Tabla del $tabla </h3>";
+ 
+ for($i=1; $i<=12; $i++) 
+ {
+  echo "$tabla x $i = ". ($tabla*$i) . "<br/>";
+ }
+}
 ?>
