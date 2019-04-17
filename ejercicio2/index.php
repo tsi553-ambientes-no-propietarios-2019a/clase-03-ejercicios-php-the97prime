a79556fdf9ad55cf9ef20f83d449cd34d4859dbc
Oscar Mateo Peñaherrera <osmap_1997@gmail.com>
2019-04-16 19:13:50 -0500

    Ejercicios 2 Titulo centrado

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,11 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+echo'<center>';
+$cadena= '<h1>Oscar Mateo</h1>';
+for($i=0;$i<=2;$i++){
+    echo "$cadena";
+    echo'<br>';
+}
+    
 ?>
